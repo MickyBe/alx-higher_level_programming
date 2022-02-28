509c8cbb003c4d505dbf75d6116bfafbdab652c5
MickyBe <bmikiyas@gmail.com>
2022-02-28 22:54:02 +0300

    ex

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,12 +4,18 @@
  * a script that prints a square
  */
   const size = Math.floor(Number(process.argv[2]));
+  let squareIndicator = "";
   if (isNaN(size)) {
     console.log('Missing size');
   } else {
-    for (let r = 0; r < size; r++) {
-      var row = "";
-      for (let c = 0; c < size; c++) {row += "X";}
-      console.log(row);
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        squareIndicator += 'X';
+      }
+      if (i !== size - 1) {
+        squareIndicator += '\n';
+      }
     }
+    console.log(squareIndicator);
+    
   }
