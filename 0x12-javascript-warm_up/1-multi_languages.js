d7c28f30ab8c1a3635c101e8780462d998625dc4
MickyBe <bmikiyas@gmail.com>
2022-02-28 22:28:27 +0300

    string case error

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,5 +3,6 @@
 /**
  * a script that prints three lines
  */
-    const myVar = "C is fun\nPython is cool\nJavaScript is amazing";
-    console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
