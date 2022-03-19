a148d08f0527ada967d98d19ac1306a2d5c33a6a
MickyBe <bmikiyas@gmail.com>
2022-03-19 22:29:32 +0300

    eficency fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm."""
-
-
+""" This function will try to find the peak element form the given list """
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """finds a peak in a list of unsorted integers"""
+    if not list_of_integers:
         return None
+    return binary_search(list_of_integers, 0, len(list_of_integers) - 1)
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
-
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+def binary_search(array, left, right):
+    """ using binary search """
+    mid = left + (right - left) // 2
+    if array[mid - 1] <= array[mid] >= array[mid + 1]:
+        return array[mid]
+    elif array[mid] > array[mid + 1]:
+        return binary_search(array, left, mid - 1)
+    elif array[mid] < array[mid + 1]:
+        return binary_search(array, mid + 1, right)
