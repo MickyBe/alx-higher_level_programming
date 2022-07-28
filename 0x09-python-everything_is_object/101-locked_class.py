116a360266597bda7654a5f0b5b1da603e54424f
MickyBe <bmikiyas@gmail.com>
2022-07-28 18:33:03 +0300

    updates

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -13,3 +13,19 @@ class LockedClass:
             self.__dict__[attribute] = value
         else:
             raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
+
+
+#or
+#!/usr/bin/python3
+"""
+    101-locked_class: class LockedClass
+"""
+
+
+class LockedClass:
+    """
+        A class that can only have one attribute first_name.
+        Attribute:
+             first_name (str): name
+    """
+    __slots__ = ['first_name']
